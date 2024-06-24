f43174ece9b97a97a30b4caa9eb93b2818b13ab7
namomi <xicodey@gmail.com>
2024-06-25 08:52:13 +0900

    [fix] AnnualLeaveControllerTest 에러 수정

--- src/test/java/com/group/hr/controller/AnnualLeaveControllerTest.java
+++ src/test/java/com/group/hr/controller/AnnualLeaveControllerTest.java
@@ -1,36 +1,60 @@
 package com.group.hr.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.group.hr.dto.AnnualLeaveDto;
 import com.group.hr.service.AnnualLeaveService;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(AnnualLeaveController.class)
-class AnnualLeaveControllerTest {
+import java.time.LocalDate;
 
-    @MockBean
-    private AnnualLeaveService annualLeaveService;
+@WebMvcTest(AnnualLeaveController.class)
+@ExtendWith(MockitoExtension.class)
+class AnnualLeaveControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private AnnualLeaveService annualLeaveService;
+
+    @InjectMocks
+    private AnnualLeaveController annualLeaveController;
+
+    private ObjectMapper objectMapper;
+    private AnnualLeaveDto annualLeaveDto;
+
+    @BeforeEach
+    public void setUp() {
+        objectMapper = new ObjectMapper();
+        annualLeaveDto = new AnnualLeaveDto(1L, LocalDate.of(2024, 2, 27), 3);
+    }
+
+
     @Test
     public void successAttendance() throws Exception {
-        // given
         Long employeeId = 1L;
 
-        // when
-        // than
-        mockMvc.perform(post("/employee/attendance/" + employeeId))
-                .andExpect(status().isOk());
+        doNothing().when(annualLeaveService).applyForLeave(employeeId, annualLeaveDto.getLeaveDate(), annualLeaveDto.getRequestedLeaves());
+
+        mockMvc.perform(post("/employee/annual-leve/{employeeId}", employeeId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(annualLeaveDto)))
+            .andExpect(status().isOk());
     }
 
     @Test
